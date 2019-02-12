b17de23a10bedca265cb124cc7dd9c91acc3116e
LahiruSasanka2 <lahirusasanka2015@gmail.com>
2019-02-12 13:58:49 +0530

    V-A-A-N-H-1.0.1

--- src/lk/ijse/dep/app/business/custom/impl/ManageItemsBOImpl.java
+++ src/lk/ijse/dep/app/business/custom/impl/ManageItemsBOImpl.java
@@ -1,60 +1,94 @@
 package lk.ijse.dep.app.business.custom.impl;
 
+import lk.ijse.dep.app.business.Converter;
 import lk.ijse.dep.app.business.custom.ManageItemsBO;
+import lk.ijse.dep.app.dao.DAOFactory;
+import lk.ijse.dep.app.dao.custom.CustomerDAO;
+import lk.ijse.dep.app.dto.CustomerDTO;
 import lk.ijse.dep.app.dto.ItemDTO;
+import lk.ijse.dep.app.util.HibernateUtil;
+import org.hibernate.Session;
 
 import java.util.List;
 
 public class ManageItemsBOImpl implements ManageItemsBO {
+
+    private CustomerDAO customerDAO;
+
+    public ManageItemsBOImpl() {
+        customerDAO = DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.CUSTOMER);
+    }
+
+
     @Override
     public List<ItemDTO> getItems() throws Exception {
-        return null;
+        Session mySession = HibernateUtil.getSessionFactory().openSession();
+        try(Session session = mySession){
+            customerDAO.setSession(session);
+            session.beginTransaction();
+            List<CustomerDTO> customerDTOS = customerDAO.findAll().map(Converter::<CustomerDTO>getDTOList).get();
+            session.getTransaction().commit();
+            return customerDTOS;
+        }catch(Exception ex){
+            mySession.getTransaction().rollback();
+            throw ex;
+        }
     }
 
     @Override
-    public boolean createItem(ItemDTO dto) throws Exception {
-        return false;
+    public void createItem(ItemDTO dto) throws Exception {
+        Session mySession = HibernateUtil.getSessionFactory().openSession();
+        try(Session session = mySession){
+            customerDAO.setSession(session);
+            session.beginTransaction();
+            customerDAO.save(Converter.getEntity(dto));
+            session.getTransaction().commit();
+        }catch(Exception ex){
+            mySession.getTransaction().rollback();
+            throw ex;
+        }
     }
 
     @Override
-    public boolean updateItem(ItemDTO dto) throws Exception {
-        return false;
+    public void updateItem(ItemDTO dto) throws Exception {
+        Session mySession = HibernateUtil.getSessionFactory().openSession();
+        try(Session session = mySession){
+            customerDAO.setSession(session);
+            session.beginTransaction();
+            customerDAO.update(Converter.getEntity(dto));
+            session.getTransaction().commit();
+        }catch(Exception ex){
+            mySession.getTransaction().rollback();
+            throw ex;
+        }
     }
 
     @Override
-    public boolean deleteItem(String code) throws Exception {
-        return false;
+    public void deleteItem(String code) throws Exception {
+        Session mySession = HibernateUtil.getSessionFactory().openSession();
+        try(Session session = mySession){
+            customerDAO.setSession(session);
+            session.beginTransaction();
+            customerDAO.delete(customerID);
+            session.getTransaction().commit();
+        }catch(Exception ex){
+            mySession.getTransaction().rollback();
+            throw ex;
+        }
     }
 
     @Override
     public ItemDTO findItem(String itemCode) throws Exception {
-        return null;
+        Session mySession = HibernateUtil.getSessionFactory().openSession();
+        try(Session session = mySession){
+            customerDAO.setSession(session);
+            session.beginTransaction();
+            CustomerDTO customerDTO = customerDAO.find(id).map(Converter::<CustomerDTO>getDTO).orElse(null);
+            session.getTransaction().commit();
+            return customerDTO;
+        }catch(Exception ex){
+            mySession.getTransaction().rollback();
+            throw ex;
+        }
     }
-
-//    private ItemDAO itemDAO;
-//
-//    public ManageItemsBOImpl() {
-//        itemDAO = DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.ITEM);
-//    }
-//
-//    public List<ItemDTO> getItems() throws Exception {
-//        return itemDAO.findAll().map(Converter::<ItemDTO>getDTOList).get();
-//    }
-//
-//    public boolean createItem(ItemDTO dto) throws Exception {
-//        return itemDAO.save(Converter.getEntity(dto));
-//    }
-//
-//    public boolean updateItem(ItemDTO dto) throws Exception {
-//        return itemDAO.update(Converter.getEntity(dto));
-//    }
-//
-//    public boolean deleteItem(String code) throws Exception {
-//        return itemDAO.delete(code);
-//
-//    }
-//
-//    public ItemDTO findItem(String itemCode) throws Exception {
-//        return itemDAO.find(itemCode).map(Converter::<ItemDTO>getDTO).orElse(null);
-//    }
 }
--- src/lk/ijse/dep/app/business/custom/impl/ManageOrdersBOImpl.java
+++ src/lk/ijse/dep/app/business/custom/impl/ManageOrdersBOImpl.java
@@ -1,12 +1,36 @@
 package lk.ijse.dep.app.business.custom.impl;
 
+import lk.ijse.dep.app.business.Converter;
 import lk.ijse.dep.app.business.custom.ManageOrdersBO;
+import lk.ijse.dep.app.dao.DAOFactory;
+import lk.ijse.dep.app.dao.custom.CustomerDAO;
+import lk.ijse.dep.app.dao.custom.ItemDAO;
+import lk.ijse.dep.app.dao.custom.OrderDAO;
+import lk.ijse.dep.app.dao.custom.OrderDetailDAO;
+import lk.ijse.dep.app.dto.CustomerDTO;
 import lk.ijse.dep.app.dto.OrderDTO;
 import lk.ijse.dep.app.dto.OrderDTO2;
+import lk.ijse.dep.app.util.HibernateUtil;
+import org.hibernate.Session;
 
 import java.util.List;
 
 public class ManageOrdersBOImpl implements ManageOrdersBO {
+
+    private CustomerDAO customerDAO;
+    private ItemDAO itemDAO;
+    private OrderDAO orderDAO;
+    private OrderDetailDAO orderDetailDAO;
+
+    public ManageOrdersBOImpl() {
+
+        customerDAO = DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.CUSTOMER);
+        itemDAO = DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.ITEM);
+        orderDAO = DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.ORDER);
+        orderDetailDAO = DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.ORDER_DETAIL);
+    }
+
+
     @Override
     public List<OrderDTO2> getOrdersWithCustomerNamesAndTotals() throws Exception {
         return null;
@@ -14,7 +38,17 @@ public class ManageOrdersBOImpl implements ManageOrdersBO {
 
     @Override
     public List<OrderDTO> getOrders() throws Exception {
-        return null;
+        Session mySession = HibernateUtil.getSessionFactory().openSession();
+        try(Session session = mySession){
+            customerDAO.setSession(session);
+            session.beginTransaction();
+            List<OrderDTO> customerDTOS = customerDAO.findAll().map(Converter::<OrderDTO>getDTOList).get();
+            session.getTransaction().commit();
+            return customerDTOS;
+        }catch(Exception ex){
+            mySession.getTransaction().rollback();
+            throw ex;
+        }
     }
 
     @Override
@@ -24,12 +58,31 @@ public class ManageOrdersBOImpl implements ManageOrdersBO {
 
     @Override
     public void createOrder(OrderDTO dto) throws Exception {
-
+        Session mySession = HibernateUtil.getSessionFactory().openSession();
+        try(Session session = mySession){
+            customerDAO.setSession(session);
+            session.beginTransaction();
+            customerDAO.save(Converter.getEntity(dto));
+            session.getTransaction().commit();
+        }catch(Exception ex){
+            mySession.getTransaction().rollback();
+            throw ex;
+        }
     }
 
     @Override
     public OrderDTO findOrder(String orderId) throws Exception {
-        return null;
+        Session mySession = HibernateUtil.getSessionFactory().openSession();
+        try(Session session = mySession){
+            customerDAO.setSession(session);
+            session.beginTransaction();
+            CustomerDTO customerDTO = customerDAO.find(id).map(Converter::<CustomerDTO>getDTO).orElse(null);
+            session.getTransaction().commit();
+            return customerDTO;
+        }catch(Exception ex){
+            mySession.getTransaction().rollback();
+            throw ex;
+        }
     }
 
 //    private OrderDAO orderDAO;
